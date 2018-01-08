2b30a3b4563660f193ad64babbf33094b78290c1
mar-bi <marina.biletska@gmail.com>
2018-01-08 17:26:14 +0200

    add link to header title

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
-import { NavLink } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 
 const Login = props => (
   <div>
@@ -47,19 +47,29 @@ const Dashboard = props => (
   <div className="banner">{props.logged ? messageUser : messageInit}</div>
 )
 
-const Header = props => (
-  <div>
-    <AppBar
-      title={<span>VotingBoom</span>}
-      iconElementRight={props.logged ? <Logged /> : <Login />}
-      showMenuIconButton={false}
-    />
-    <Dashboard logged={props.logged} />
-  </div>
-)
+const Header = props => {
+  const titleClick = () => props.history.push('/')
+  
+  return (
+    <div>
+      <AppBar
+        title={<span
+          className="header-title"
+          onClick={titleClick}
+          >
+            VotingBoom
+          </span>}
+        iconElementRight={props.logged ? <Logged /> : <Login />}
+        showMenuIconButton={false}
+
+      />
+      <Dashboard logged={props.logged} />
+    </div>
+  )
+}
 
 Header.defaultProps = {
   logged: false
 }
 
-export default Header
+export default withRouter(Header)
