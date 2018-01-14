332900491166038c7641150ee45213aae20fb88c
mar-bi <marina.biletska@gmail.com>
2018-01-14 15:30:50 +0200

    update pollresult component

--- app/components/PollResults.js
+++ app/components/PollResults.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Divider from 'material-ui/Divider'
 import Paper from 'material-ui/Paper'
 
+// get access to the data (votes) for particular poll and build the results
+// using D3 library (simple barchart) 
 const PollResults = props => (
   <div className="poll-results-container">
     <Paper zDepth={2}>
-      <h3>Here is the results</h3>
+      <h3>{props.match.params.pollId} <br/> <span>Poll Results</span></h3>
+      <div>
+        Here is the place for a graph.
+      </div>
     </Paper>
   </div>
 )
