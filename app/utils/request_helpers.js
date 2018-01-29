094c95e3d6fcd27639c13198b8e30f1689ccae40
mar-bi <marina.biletska@gmail.com>
2018-01-29 17:20:59 +0200

    update requests to api

--- app/utils/request_helpers.js
+++ app/utils/request_helpers.js
@@ -1,11 +1,34 @@
 import axios from 'axios'
+import Auth from './Auth'
 
+//!!! change before deployment
 const home = 'http://localhost:3000/'
 
 // callback is for purpose of redirection
-// create poll and vote
-export function sendPollData(str, data, callback) {
-  const url = home + str
+// request from PollForm component
+export function createPoll(data, callback) {
+  const url = `${home}api/private/addPoll`
+  const config = {
+    headers: {'Authorization': `bearer ${Auth.getToken()}`}
+  }
+
+  axios
+    .post(url, data, config)
+    .then(function(response) {
+      if (callback) {
+        callback(response.data)
+      }
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+}
+
+// callback is for purpose of redirection
+// request from SinglePoll component
+export function sendVote(data, callback) {
+  const url = `${home}api/public/addVote`
+
   axios
     .post(url, data)
     .then(function(response) {
@@ -18,12 +41,38 @@ export function sendPollData(str, data, callback) {
     })
 }
 
+// requests from SignupForm and LoginFrom Components
 // callback is for purpose of redirection
 export function sendAuthData(str, data, errorCB, successCB) {
-  const url = home + '/auth/' + str
+  const url = home + 'auth/' + str
   axios
     .post(url, data)
     .then(function(response) {
+      //@response.data {object {success, token, user, path}}
+      //console.log(response)
+      if (!response.data.success){
+        errorCB(response.data)
+      } else {
+        //console.log(response.data)
+        successCB(response.data)
+      }
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+}
+
+// requests from PasswordForm
+export function changePassword(data, errorCB, successCB) {
+  const url = `${home}api/private/changePassword`
+  const config = {
+    headers: {'Authorization': `bearer ${Auth.getToken()}`}
+  }
+
+  axios
+    .post(url, data, config)
+    .then(function(response) {
+      //@response.data {object {success, message}}
       //console.log(response)
       if (!response.data.success){
         errorCB(response.data)
@@ -41,9 +90,12 @@ export function getPolls(user, callback) {
   const url = user
     ? home + `api/private/${user}/getpolls`
     : home + 'api/public/getpolls'
-  //console.log(url)
+  const config = user
+    ? { headers: {'Authorization': `bearer ${Auth.getToken()}`} }
+    : {}
+
   axios
-    .get(url)
+    .get(url, config)
     .then(function(response) {
       //console.log(response.data)
       callback(response.data)
@@ -69,6 +121,10 @@ export function getPoll(name, callback) {
 // callback is for purpose of redirection
 export function deletePoll(data, callback) {
   const url = `${home}api/private/deletePoll`
+  const config = {
+    headers: {'Authorization': `bearer ${Auth.getToken()}`}
+  }
+
   axios
     .post(url, data)
     .then(function(response) {
