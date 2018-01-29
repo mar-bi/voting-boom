f222d7d4a8affcde8fbe1da3de07dcacc841e8eb
mar-bi <marina.biletska@gmail.com>
2018-01-29 17:22:43 +0200

    add method to store user

--- app/utils/Auth.js
+++ app/utils/Auth.js
@@ -1,7 +1,8 @@
 class Auth {
   //Authenticate a user. Save a token string in Local Storage
   // @param {string} token
-  static authenticateUser(token) {
+  static authenticateUser(token, user) {
+    localStorage.setItem('user', user.name);
     localStorage.setItem('token', token);
   }
 
@@ -14,12 +15,20 @@ class Auth {
   //Deauthenticate a user. Remove a token from Local Storage.
   static deauthenticateUser() {
     localStorage.removeItem('token');
+    localStorage.removeItem('user');
   }
 
   //Get a token value.
   // @returns {string}
   static getToken() {
     return localStorage.getItem('token');
+  }
+
+
+  //Get a user value.
+  // @returns {string}
+  static getUser() {
+    return localStorage.getItem('user');
   }
 }
 
