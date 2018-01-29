d807d09ae18089b920b203346bec0d83c66d799a
mar-bi <marina.biletska@gmail.com>
2018-01-29 17:25:41 +0200

    add private route to app

--- app/app.js
+++ app/app.js
@@ -10,13 +10,30 @@ import AllPolls from './components/AllPolls'
 import UserTabs from './components/UserTabs'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
+import PasswordForm from './components/PasswordForm'
 import SinglePoll from './components/SinglePoll'
 import PollResults from './components/PollResults'
 import NotFoundPage from './components/NotFoundPage'
 import PollAddress from './components/PollAddress'
 
+import Auth from './utils/Auth'
+
 // import react router deps
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    Auth.isUserAuthenticated() ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
 
 const App = () => (
   <Router>
@@ -27,7 +44,8 @@ const App = () => (
           <Route exact path="/" component={AllPolls} />
           <Route path="/login" component={LoginForm} />
           <Route path="/signup" component={SignupForm} />
-          <Route path="/user/:userId" component={UserTabs} />
+          <Route path="/changepassword" component={PasswordForm} />
+          <PrivateRoute path="/user/:userId" component={UserTabs} />
           <Route path="/polls/results/:pollId" component={PollResults} />
           <Route exact path="/polls/link/:pollId" component={PollAddress} />
           <Route exact path="/polls/:pollId" component={SinglePoll} />
